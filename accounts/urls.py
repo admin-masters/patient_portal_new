e7b04471626a2154b145633a0059dfb2fcdf7cdb
Niomi Samani <94210866+niomisamani@users.noreply.github.com>
2025-12-23 15:26:47 +0530

    Update URL patterns for password reset and modify clinic

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
-
 from . import views
 
 app_name = "accounts"
 
 urlpatterns = [
     path("register/", views.register_doctor, name="register"),
+    path("modify/<str:doctor_id>/", views.modify_clinic_details, name="modify_clinic_details"),
     path("login/", views.doctor_login, name="login"),
     path("logout/", views.doctor_logout, name="logout"),
-    path("forgot/", views.request_password_reset, name="forgot"),
+    path("request-password-reset/", views.request_password_reset, name="request_password_reset"),
     path("reset/<uidb64>/<token>/", views.password_reset, name="password_reset"),
 ]
